8482a2db0d7e792dffda9b54033cd6c2bb3821d5
Gerry Waterston <gwaterston@hotmail.co.uk>
2025-03-16 13:12:35 +0000

    Creates html reports and screenshots.

--- playwright-automation-framework/src/index.ts
+++ playwright-automation-framework/src/index.ts
@@ -8,6 +8,7 @@ const common = `./src/features/*.feature \
  --require ./src/step-definitions/**/**/*.ts \
  --require ./src/utils/cucumber-timeout.ts \
  -f json:./reports/report.json \
+ --format html:./reports/report.html \
  --tags "not @ignore"`;
 
 //  Define an interface for the profiles object
